034acefaeb8141040de488c356dd50e5feb30066
Gabriel Nogueira Vieira Resende <gabrielnvresende@gmail.com>
2025-06-24 12:25:28 -0300

    hahahahahahhaahhaahahahahahhsahhahahfeonfjwenf

--- autograder.py
+++ autograder.py
@@ -1,6 +1,6 @@
 
 print("I am the autograder script. Behold my power!")
-with open("./node_test_output.txt", "r") as file:
+with open("/github/workspace/node_test_output.txt", "r") as file:
     content = file.read()
     print(content)
 print("I am the autograder script. I have finished running!")
