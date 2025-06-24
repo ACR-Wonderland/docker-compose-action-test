7294135972f8a591748b17c6090c62497a08d23e
ArthurCRodrigues <arthurcarvalhorodrigues2409@gmail.com>
2025-06-24 16:53:05 -0300

    gabigol, eis o leite derramado.

--- autograder.py
+++ autograder.py
@@ -1,5 +1,5 @@
 import os
-
+print("I am the autograder script. Behold my greatness!")
 submission_dir = '/submission'
 
 if os.path.exists(submission_dir) and os.path.isdir(submission_dir):
@@ -7,3 +7,5 @@ if os.path.exists(submission_dir) and os.path.isdir(submission_dir):
         print(filename)
 else:
     print(f"Directory '{submission_dir}' does not exist.")
+    
+print("That's all, folks!")
